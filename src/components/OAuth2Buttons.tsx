c9db3c177e0e86fbad176bc4698a5c2176ac2abf
Jaypal Koli <kolijaypal47@gmail.com>
2026-02-07 16:50:16 +0530

    introduce OAuth2Buttons component for Google and GitHub login.

--- src/components/OAuth2Buttons.tsx
+++ src/components/OAuth2Buttons.tsx
@@ -1,29 +1,29 @@
 
 import { Button } from "./ui/button";
 import { Github, Mail } from "lucide-react";
-import { NavLink } from "react-router";
+
 
 function OAuth2Buttons() {
   return (
     <div className="grid gap-3">
-      <NavLink
-        to={`${
+      <a
+        href={`${
           import.meta.env.VITE_BASE_URL || ""
         }/oauth2/authorization/google`}
       >
         <Button variant="outline" className="w-full flex items-center gap-2 cursor-pointer">
           <Mail className="h-4 w-4" /> Continue with Google
         </Button>
-      </NavLink>
-      <NavLink
-        to={`${
+      </a>
+      <a
+        href={`${
           import.meta.env.VITE_BASE_URL || ""
         }/oauth2/authorization/github`}
       >
         <Button variant="outline" className="w-full flex items-center gap-2 cursor-pointer">
           <Github className="h-4 w-4" /> Continue with GitHub
         </Button>
-      </NavLink>
+      </a>
     </div>
   );
 }
