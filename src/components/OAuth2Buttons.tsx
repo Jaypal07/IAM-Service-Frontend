d23699d01bff4fd610f035391c0743fbc9647136
Jaypal Koli <kolijaypal47@gmail.com>
2026-02-08 01:28:30 +0530

    add OAuth2 authentication proxy relative url

--- src/components/OAuth2Buttons.tsx
+++ src/components/OAuth2Buttons.tsx
@@ -7,14 +7,14 @@ function OAuth2Buttons() {
   return (
     <div className="grid gap-3">
       <a
-        href={`${import.meta.env.VITE_BASE_URL}/oauth2/authorization/google`}
+        href="/oauth2/authorization/google"
       >
         <Button variant="outline" className="w-full flex items-center gap-2 cursor-pointer">
           <Mail className="h-4 w-4" /> Continue with Google
         </Button>
       </a>
       <a
-        href={`${import.meta.env.VITE_BASE_URL}/oauth2/authorization/github`}
+        href="/oauth2/authorization/github"
       >
         <Button variant="outline" className="w-full flex items-center gap-2 cursor-pointer">
           <Github className="h-4 w-4" /> Continue with GitHub
