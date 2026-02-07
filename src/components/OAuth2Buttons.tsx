98775050b2d71831ccb1628c3162a565126670fd
Jaypal Koli <kolijaypal47@gmail.com>
2026-02-08 00:49:02 +0530

    changed

--- src/components/OAuth2Buttons.tsx
+++ src/components/OAuth2Buttons.tsx
@@ -7,14 +7,14 @@ function OAuth2Buttons() {
   return (
     <div className="grid gap-3">
       <a
-        href="http://3.110.155.78.nip.io:8080/oauth2/authorization/google"
+        href="/oauth2/authorization/google"
       >
         <Button variant="outline" className="w-full flex items-center gap-2 cursor-pointer">
           <Mail className="h-4 w-4" /> Continue with Google
         </Button>
       </a>
       <a
-        href="http://3.110.155.78.nip.io:8080/oauth2/authorization/github"
+        href="/oauth2/authorization/github"
       >
         <Button variant="outline" className="w-full flex items-center gap-2 cursor-pointer">
           <Github className="h-4 w-4" /> Continue with GitHub
